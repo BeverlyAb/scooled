f8fbd6791010836b0945884b8b921e8b9a41043e
babadine <bev.abadines@gmail.com>
2021-08-26 10:34:41 -0700

    created assignments_table

--- scooled/teacher_page.py
+++ scooled/teacher_page.py
@@ -12,14 +12,16 @@ class Teacher:
         self.name = name
         # self.df = df
 
-    def display(self, courses)->None:
+    def display(self, courses, assignments)->None:
         st.title("s'cooled")
         st.sidebar.title(f'Welcome, {self.name}')
-        st.sidebar.selectbox(label='Courses',options=courses)
+        course = st.sidebar.selectbox(label='Courses',options=courses)
+        st.stop()
+        self.assignments(course,assignments)
 
+    def assignments(self,course,assignments)->None:
+        st.write
 
-    def assignments(self,course,students)->None:
-        st.write(students)
 
 
 
@@ -30,8 +32,11 @@ if __name__ == '__main__':
     students = [str(name) for name in range(100)]
     grades = [grade for grade in map(lambda x: random.randrange(0,101),range(len(students)*len(courses)))]
     assignments = [assignment[0]+'_'+str(assignment[1]) for assignment in itertools.product(courses,range(10))]
-    assignment_table = [table for table in itertools.product([courses[0]],assignments[0:10])]
-    st.write(assignment_table)
+    
+    assignment_table = []
+    for i in range(len(courses)):
+        assignment_table.append(pd.Series([table for table in assignments[i*10:(i+1)*10]],name=courses[i]))
+    st.write(pd.DataFrame(assignment_table))
     teacher  = Teacher('Bev')
-    teacher.display(courses)
+    teacher.display(courses,assignments)
 
