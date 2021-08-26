c068ab7008162d4de90af9385e794500d43cace8
babadine <bev.abadines@gmail.com>
2021-08-26 11:03:36 -0700

    whoo! can add to specific assignment

--- scooled/teacher_page.py
+++ scooled/teacher_page.py
@@ -19,7 +19,9 @@ class Teacher:
         self.assignments(course,assignment_table)
 
     def assignments(self,course,assignment_table)->None:
-        st.write(assignment_table[course])
+        assignments = assignment_table[course]   
+        st.write(assignment_table, assignments)
+        st.sidebar.selectbox(label='Assignment',options=assignments)
 
 #outside of class
 def gen_dummy():
@@ -27,12 +29,19 @@ def gen_dummy():
     students = [str(name) for name in range(100)]
     grades = [grade for grade in map(lambda x: random.randrange(0,101),range(len(students)*len(courses)))]
     assignments = [assignment[0]+'_'+str(assignment[1]) for assignment in itertools.product(courses,range(10))]
-    
-    assignment_table = []
+     
+    # assignment_table = []
     for i in range(len(courses)):
-        assignment_table.append(pd.Series([table for table in assignments[i*10:(i+1)*10]],name=courses[i]))
-    assignment_table = pd.DataFrame(assignment_table)
-    assignment_table = assignment_table.transpose()
+        assign_name_series = pd.Series([table for table in assignments[i*10:(i+1)*10]],name=courses[i])
+        assign_desc_series = pd.Series(['Description for '+table for table in assign_name_series],name=courses[i]+"_description")
+        # assignment_table.append(pd.Series([table for table in assignments[i*10:(i+1)*10]],name=courses[i]))
+    st.write(assign_name_series,assign_desc_series)
+    # assignment_table = pd.DataFrame(assignment_table)
+    # assignment_table = assignment_table.transpose()
+
+    assignment_table = pd.DataFrame([assign_name_series,assign_desc_series]).transpose()
+    st.write(assignment_table)
+    st.stop()
 
     return courses, students, grades, assignment_table
 
