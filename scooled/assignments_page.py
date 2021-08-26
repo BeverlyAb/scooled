c8aca58c99360869dbd4359953fcba63aeae7399
babadine <bev.abadines@gmail.com>
2021-08-26 14:05:59 -0700

    added assignments_page

---
+++ scooled/assignments_page.py
@@ -0,0 +1,12 @@
+# assignments_page.py
+
+import streamlit as st
+from structs import PageType as pt
+
+class Assignments:
+
+    def __init__(self) -> None:
+        self.assignment = st.session_state[pt.assign]
+
+    def display(self):
+        st.write(self.assignment)
