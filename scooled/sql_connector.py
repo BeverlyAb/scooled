c8dc0fae88885e5af9275d4ab372983f4c0b2495
babadine <bev.abadines@gmail.com>
2021-08-27 16:54:18 -0700

    class works

--- scooled/sql_connector.py
+++ scooled/sql_connector.py
@@ -1,16 +1,33 @@
 import psycopg2
-import csv
 import streamlit as st
 import os
 
-# conn = psycopg2.connect(database = st.secrets["name_of_db"],user = st.secrets["username"] ,host = st.secrets["hostname"], port=st.secrets["port_number"] ,password = st.secrets["user_password"])
-conn = psycopg2.connect(database = os.environ["db"],user = os.environ["user"] ,host = os.environ["hostname"] ,password = os.environ["password"], port = os.environ["port"])
-st.write("HELLO")
-cur = conn.cursor()
-cur.execute("USE test;")
-cur.execute("SELECT * FROM test.courses WHERE name in ('ENGLISH_0');")
-for i in cur:
-    st.write("HI",i)
-st.write(cur.execute("SHOW TABLES"))
 
-st.stop()
+class SQLConnector():
+    def __init__(self):
+        # I am the only one using this at this point 
+        self.conn = psycopg2.connect(database = os.environ["db"],user = os.environ["user"] ,host = os.environ["hostname"] ,password = os.environ["password"], port = os.environ["port"])
+        self.cur = self.conn.cursor()
+        self.cur.execute("USE test;")
+
+    def __del__(self):
+        del self.conn
+        del self.cur
+
+    def get(self,table : str, get_cols : list, from_col : str, val_from_col : str ):
+        table = 'test.' + table
+        self.cur.execute(f"SELECT {','.join(get_cols)} FROM {table} WHERE {from_col} in ('{val_from_col}');")
+        for i in self.cur:
+            st.write("HI",i)
+
+
+if __name__ == "__main__":
+    sql_con = SQLConnector()
+    get_cols = ['teacher','student']
+    table = 'courses'
+    from_col = 'name'
+    val_from_col = 'ENGLISH_0'
+    sql_con.get(table,get_cols,from_col,val_from_col)
+# st.write(cur.execute("SHOW TABLES"))
+
+# st.stop()
