0c6915d0d518e24494d1ba2165d01294b40518ff
babadine <bev.abadines@gmail.com>
2021-08-27 17:04:51 -0700

    returning dataframe

--- scooled/sql_connector.py
+++ scooled/sql_connector.py
@@ -1,6 +1,7 @@
 import psycopg2
 import streamlit as st
 import os
+import pandas as pd
 
 
 class SQLConnector():
@@ -14,12 +15,26 @@ class SQLConnector():
         del self.conn
         del self.cur
 
+    def query(self, command : str):
+        self.cur.execute(command)
+
     def get(self,table : str, get_cols : list, from_col : str, val_from_col : str ):
+        '''get certain col(s) based on certain col value'''
         table = 'test.' + table
         self.cur.execute(f"SELECT {','.join(get_cols)} FROM {table} WHERE {from_col} in ('{val_from_col}');")
-        for i in self.cur:
-            st.write("HI",i)
+        
+        out = pd.Series()   
+        out = [val for val in self.cur]
+        return out
 
+    def get(self,table : str ):
+        '''get everything'''
+        table = 'test.' + table
+        self.cur.execute(f"SELECT * FROM {table};")
+        
+        out = pd.DataFrame()   
+        out = [val for val in self.cur]
+        return out
 
 if __name__ == "__main__":
     sql_con = SQLConnector()
@@ -27,7 +42,9 @@ if __name__ == "__main__":
     table = 'courses'
     from_col = 'name'
     val_from_col = 'ENGLISH_0'
-    sql_con.get(table,get_cols,from_col,val_from_col)
+    # out= sql_con.get(table,get_cols,from_col,val_from_col)
+    out = sql_con.get(table)
+    st.write(out)
 # st.write(cur.execute("SHOW TABLES"))
 
 # st.stop()
