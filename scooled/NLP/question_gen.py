001d5711e0508ff6f4d32f8e021c38cda6130119
babadine <bev.abadines@gmail.com>
2021-09-12 09:19:44 -0700

    comments

--- scooled/NLP/question_gen.py
+++ scooled/NLP/question_gen.py
@@ -11,9 +11,27 @@ class QuestionGen:
        nltk.download('stopwords') # python -m nltk.downloader stopwords 
 
     def generate(self,payload :dict):
+        """Runs NLP model and returns dict of questions, answer, option, and context
+
+        Args:
+            payload (dict): [description]
+
+        Returns:
+            output: list
+        """        
         output = self.qg.predict_mcq(payload)
         return self.q_and_a(output)
+    
     def q_and_a(self,bank : dict):
+        """splits bank into question, answer, opts, and note
+
+        Args:
+            bank (dict): 
+
+        Returns:
+            list: 
+        """       
+        question = bank['questions'][0]['question_statement'] 
         answer = bank['questions'][0]['answer']
         opt1 = bank['questions'][0]['options'][0]
         opt2 = bank['questions'][0]['options'][0]
@@ -21,12 +39,12 @@ class QuestionGen:
         opt4 = answer
         note = bank['questions'][0]['context']
 
-        return answer, [opt1,opt2,opt3,opt4],note
+        return question,answer, [opt1,opt2,opt3,opt4],note
 
 if __name__=="__main__":
     test = QuestionGen()
     payload = {
             "input_text": "Sachin Ramesh Tendulkar is a former international cricketer from India and a former captain of the Indian national team."
     }
-    ans, opt_list, note = test.generate(payload)
-    print(ans,'\n',opt_list,note,'\n')
+    ques, ans, opt_list, note = test.generate(payload)
+    print(ques,'\n',ans,'\n',opt_list,note,'\n')
